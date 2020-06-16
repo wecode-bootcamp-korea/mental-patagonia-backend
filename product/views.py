cad79d638422973720b9c09d5b84998ba716f717
youngbinha <guseowhtjs@gmail.com>
2020-06-16 20:20:36 +0900

    [FIX]pagination 기능 준비

--- product/views.py
+++ product/views.py
@@ -7,6 +7,8 @@ from .models import Product, ProductSize, ColorProduct, ColorProductImage, Color
 
 class ProductView(View):
     def get(self, request):
+        offset = int(request.GET.get('offset',0))
+        limit  = int(request.GET.get('limit', 60))
         all_product = Product.objects.prefetch_related('subcategoryproduct_set','colorproduct_set').all()
 
         products=[{
@@ -23,6 +25,11 @@ class ProductView(View):
             'product_image' : prod.colorproduct_set.get(is_default_image=True).image_url,
             'hover_image'   : [image.image_url for image in prod.colorproduct_set.filter(is_default_image=True) if image != None],
             'defalut_color' : str(prod.colorproduct_set.filter(is_default_image=True).first().color.code),
-        } for prod in all_product]
+        } for prod in all_product[offset*limit : (offset+1) * limit]]
+
 
         return JsonResponse({'data':products}, status=200)
+
+class MainCategoryView(View):
+    def get(self, request):
+        all_category    = Main
