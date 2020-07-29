df9bf95483945772a67333259ef273537596af23
youngbinha <guseowhtjs@gmail.com>
2020-07-29 16:15:42 +0900

    add MypageView

--- member/urls.py
+++ member/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import KakaoSignInCallbackView
+from .views import KakaoSignInCallbackView, MypageMainView
 
 urlpatterns = [
     path('/kakao', KakaoSignInCallbackView.as_view()),
+    path('/', MypageMainView.as_view())
 ]
--- member/views.py
+++ member/views.py
@@ -11,55 +11,88 @@ from member.models      import Member
 from django.views       import View
 from django.http        import JsonResponse, HttpResponse
 
+class LoginConfirm:
+    def __init__(self,original_function):
+        self.original_function = original_function
+
+    def __call__(self, request, *args, **kwargs):
+        token = request.headers.get("Authorization", None)
+        print(10)
+        try:
+            if token:
+                print(20)
+                token_payload	= jwt.decode(token, SECRET_KEY, ALGORITHM)
+                user			= Member.objects.get(email=token_payload['email'])
+                request.user	= user
+                return self.original_function(self, request, *args, **kwargs)
+
+            return JsonResponse({'message':'INVALID_USER'}, status=401)
+        except jwt.DecodeError:
+            return JsonResponse({'message':'INVALID_TOKEN'}, status=401)
+
+        except Member.DoesNotExist:
+            return JsonResponse({'message':'INVALID_USER'}, status=401)
+
 class KakaoSignInCallbackView(View):
     def get(self, request):
         try:
             access_token = request.headers.get("Authorization", None)
+            if type(access_token) == type(None):
+                return JsonResponse({"message" : "ACCESS_DENIED"}, status = 401)
+
             profile_request = requests.get(
                 "https://kapi.kakao.com/v2/user/me", headers={"Authorization" : f"Bearer {access_token}"},
             )
+            print(profile_request.json())
+            profile_json    = profile_request.json().get("kakao_account",None)
+            print(profile_json)
+            email           = profile_json.get("email", None)
+            fullname        = profile_json.get("profile", None).get("nickname", None)
 
-            profile_json = profile_request.json()
-            kakao_account = profile_json.get("kakao_account")
-            email = kakao_account.get("email", None)
-            kakao_id = profile_json.get("id")
             if Member.objects.filter(email = email).exists():
                 member = Member.objects.get(email=email)
                 token = jwt.encode({"email" : member.email}, SECRET_KEY, ALGORITHM)
                 token = token.decode("utf-8")
                 return JsonResponse({"token" : token ,"message" : "SUCCESS"}, status=200)
             else:
-                firstname = kakao_account.get("profile", None).get("nickname", None)
-                email = kakao_account.get("email", None)
                 Member.objects.create(
                     email = email,
-                    firstname = firstname[0],
-                    lastname  = firstname[1:]
+                    firstname = fullname[0],
+                    lastname  = fullname[1:]
                 )
             token = jwt.encode({"email" : email}, SECRET_KEY, ALGORITHM)
             token = token.decode("utf-8")
 
+            return JsonResponse({"token" : token ,"message" : "SUCCESS"}, status = 200)
+
         except KeyError:
             return JsonResponse({"message" : "INVALID_TOKEN"}, status = 400)
 
-        except access_token.DoesNotExist:
-            return JsonResponse({"message" : "INVALID_TOKEN"}, status = 400)
-
-            return JsonResponse({"token" : token ,"message" : "SUCCESS"}, status = 200)
-
-def login_decorator(func):
-
-    def wrapper(self, request, *args, **kwargs):
+class MypageMainView(View):
+    @LoginConfirm
+    def get(self, request):
         try:
-            auth_token = request.headers.get('Authorization', None)
-            payload    = jwt.decode(auth_token, SECRET_KEY, ALGORITHM)
+            member      = request.user
+            address     = member.addressbook_set.filter(is_default=True)
+            member_info = {
+                "firstname" : member.firstname,
+                "lastname"  : member.lastname,
+                "email"     : member.email
+            }
+            if address:
+                address_book = {
+                    "name"          : address.name,
+                    "firstname"     : address.firstname,
+                    "lastname"      : address.lastname,
+                    "address1"      : address.address1,
+                    "address2"      : address.address2,
+                    "city"          : address.city,
+                    "zipcode"       : address.zipcode,
+                    "phone_number"  : address.phone_number,
+            }
+            else:
+                address_book = {}
+            return JsonResponse({"member_info" : member_info, "address_book" : address_book}, status=200)
+        except KeyError:
+            return JsonResponse({"message" : "INVALID_KEY"}, status=400)
 
-            request.member = Member.objects.get(id=payload['member_id'])
-            return func(self, request, *args, **kwargs)
-
-        except Member.DoesNotExist:
-            return JsonResponse({"message" : "INVALID_USER"}, status=400)
-        except jwt.exceptions.DecodeError:
-            return JsonResponse({"message" : "INVALID_TOKEN"}, status=400)
-
-    return wrapper
